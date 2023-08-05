fcb30acf0f1d8380c5081a380e8c5a02bf3f55b3
reesmcivor <reesmcivor@hotmail.com>
2023-08-05 05:28:16 +0100

    Auto

---
+++ src/Exceptions/NoChatGptQuery.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ReesMcIvor\ChatGPT\Exceptions;
+
+use Exception;
+
+class NoChatGptQuery extends Exception {
+
+}
--- src/Services/ChatGPT.php
+++ src/Services/ChatGPT.php
@@ -2,10 +2,39 @@
 
 namespace ReesMcIvor\ChatGPT\Services;
 
+use Illuminate\Support\Facades\Http;
+
 class ChatGPT {
 
-    public function __construct() {
-        dd('got here');
+    public function chat( $message ) {
+
+        if(!$message) {
+            throw new NoChatGptQuery("No message provided");
+        }
+
+        $data = Http::withHeaders([
+            'Content-Type' => 'application/json',
+            'Authorization' => 'Bearer '.config('chat-gpt.api_secret'),
+        ])
+            ->post("https://api.openai.com/v1/chat/completions", [
+                "model" => "gpt-3.5-turbo",
+                'messages' => [
+                    [
+                        "role" => "user",
+                        "content" => $message
+                    ]
+                ],
+                'temperature' => 0.5,
+                "max_tokens" => 200,
+                "top_p" => 1.0,
+                "frequency_penalty" => 0.52,
+                "presence_penalty" => 0.5,
+                "stop" => ["11."],
+            ])
+            ->json();
+
+        return $data['choices'][0]['message'];
+
     }
 
 }
